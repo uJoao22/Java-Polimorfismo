9788a421143b5b01680530843ad5156d0568e3da
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2021-12-28 14:40:50 -0300

    Herança e construtores

---
+++ bytebank-herdado-conta/src/Conta.java
@@ -0,0 +1,78 @@
+public class Conta {
+	private double saldo;
+	private int agencia;
+	private int numero;
+	
+	//Criando a referencia para o objeto Cliente
+	private Cliente titular;
+	
+	private static int total;
+	
+	
+	public Conta(int agencia, int numero) {
+		Conta.total++;
+		this.agencia = agencia;
+		this.numero = numero;
+	}
+	
+	public void deposita(double valor){
+		this.saldo += valor;
+	}
+	
+	public boolean saca(double valor) {
+		if(valor <= this.saldo) {
+			this.saldo -= valor; 
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean transfere(double valor, Conta destino) {
+		boolean res = saca(valor);
+		if(res) {
+			destino.deposita(valor);
+			return true;
+		}
+		return false;
+	}
+
+	public double getSaldo() {
+		return this.saldo;
+	}
+
+	public int getAgencia() {
+		return this.agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		if(agencia <= 0) {
+			System.out.println("Agencia não pode ser menor ou igual a 0");
+			return;
+		}
+		this.agencia = agencia;
+	}
+
+	public int getNumero() {
+		return this.numero;
+	}
+
+	public void setNumero(int numero) {
+		if(numero <= 0) {
+			System.out.println("O número da conta não pode ser menor ou igual a 0");
+			return;
+		}
+		this.numero = numero;
+	}
+
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}	
+	
+	public Cliente getTitular() {
+		return titular;
+	}
+	
+	public static int getTotal() {
+		return total;
+	}
+}
