12b6d6ad094501b77b1f53b9c41b8840441f7ad8
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2021-12-28 13:47:13 -0300

    Aplicando Polimorfismo

--- bytebank-herdado/src/TesteReferencias.java
+++ bytebank-herdado/src/TesteReferencias.java
@@ -1,12 +1,22 @@
 public class TesteReferencias {
 	public static void main(String[] args) {
 
-		Gerente g1 = new Gerente();
-		
+		Funcionario g1 = new Gerente();
 		g1.setNome("Marcos");
-		String nome = g1.getNome();
+		g1.setSalario(5000.0);
 		
-		System.out.println(nome);
+		Funcionario f1 = new Funcionario();
+		f1.setSalario(2000.0);
 		
+		Funcionario ev = new EditorVideo();
+		ev.setSalario(2500.0);
+		
+		ControleBonificacao controle = new ControleBonificacao();
+		
+		controle.registra(g1); //Gerente
+		controle.registra(f1); //Funcionário
+		controle.registra(ev); //Editor de vídeo
+		
+		System.out.println(controle.getSoma());
 	}
 }
