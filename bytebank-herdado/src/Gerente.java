5628966927e7e332839ea6c5beae7cfe25632423
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2021-12-29 10:26:25 -0300

    Mais uma classe abstrata

--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -1,17 +1,4 @@
-public class Gerente extends Funcionario{
-	
-	private int senha;
-	
-	public boolean autentica(int senha) {
-		if(this.senha == senha)
-			return true;
-		else
-			return false;
-	}
-	
-	public void setSenha(int senha) {
-		this.senha = senha;
-	}
+public class Gerente extends FuncionarioAutenticavel{
 	
 	public double getBonificacao() {
 		return super.getSalario();
